528ed8dbf6b3ca4bdfb9f1ad3d8d50b9db8defee
Roberto Tirado Magallanes <rtmag4@gmail.com>
2017-10-11 17:55:45 +0800

    Create methylKit_dmc.R

---
+++ methylKit_dmc.R
@@ -0,0 +1,7 @@
+library(methylKit)
+
+file.list=list( "HCT116.Decitabine.WGBS.run1.1_bismark_bt2_pe.CpG_report.txt",                
+                "HCT116.Decitabine.WGBS.run2.1_bismark_bt2_pe.CpG_report.txt",
+                "HCT116.control.WGBS.run1.1_bismark_bt2_pe.CpG_report.txt",
+                "HCT116.control.WGBS.run2.1_bismark_bt2_pe.CpG_report.txt" )
+
