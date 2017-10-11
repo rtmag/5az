c8bc83ed490e33c7217880394640c0c9c8b406e6
Roberto Tirado Magallanes <rtmag4@gmail.com>
2017-10-11 18:27:49 +0800

    Update 1.trim.sh

--- 1.trim.sh
+++ 1.trim.sh
@@ -1,10 +1,16 @@
 
 trim_galore --illumina --paired -o /root/wgbs/fastq_trim \
 /root/wgbs/HCT116.Decitabine.WGBS.run1.1.fastq.gz \
-/root/wgbs/HCT116.Decitabine.WGBS.run1.2.fastq.gz \
+/root/wgbs/HCT116.Decitabine.WGBS.run1.2.fastq.gz &
+
+trim_galore --illumina --paired -o /root/wgbs/fastq_trim \
 /root/wgbs/HCT116.Decitabine.WGBS.run2.1.fastq.gz  \
-/root/wgbs/HCT116.Decitabine.WGBS.run2.2.fastq.gz  \
+/root/wgbs/HCT116.Decitabine.WGBS.run2.2.fastq.gz &  
+
+trim_galore --illumina --paired -o /root/wgbs/fastq_trim \
 /root/wgbs/HCT116.control.WGBS.run1.1.fastq.gz  \
-/root/wgbs/HCT116.control.WGBS.run1.2.fastq.gz  \
+/root/wgbs/HCT116.control.WGBS.run1.2.fastq.gz  &
+
+trim_galore --illumina --paired -o /root/wgbs/fastq_trim \
 /root/wgbs/HCT116.control.WGBS.run2.1.fastq.gz  \
-/root/wgbs/HCT116.control.WGBS.run2.2.fastq.gz  
+/root/wgbs/HCT116.control.WGBS.run2.2.fastq.gz  &
