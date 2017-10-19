326813626cbcac6f2371de5fa852fb750a5f0853
Roberto Tirado Magallanes <rtmag4@gmail.com>
2017-10-19 17:37:59 +0200

    Update 3.computeMatrix_DMC.sh

--- 3.computeMatrix_DMC.sh
+++ 3.computeMatrix_DMC.sh
@@ -1,10 +1,12 @@
 cut -f 1,2,3 fisher_neb_diff.txt > fisher_neb_diff_und.bed
-echo "# Diff Methylated" >> fisher_neb_diff_und.bed
+echo "# Differentially Methylated" >> fisher_neb_diff_und.bed
 cut -f 1,2,3 fisher_neb_undiff.txt >> fisher_neb_diff_und.bed
-echo "# UnDiff Methylated" >> fisher_neb_diff_und.bed
+echo "# Not-differentially Methylated" >> fisher_neb_diff_und.bed
 
 
 computeMatrix reference-point \
--S atac_rmdup.bw P53_Venkata.bw P53_24h_doxo_s1.bw P53_48h_doxo_s1.bw H3K27ac.bw H3K4me1.bw H3K4me3.bw H3K27me3.bw H3K9me3.bw H3K36me3.bw  \
--R fisher_neb_diff_und.bed --referencePoint center \
---sortRegions descend -a 1000 -b 1000 -p 40 -out fisher_neb_diff_und.mat
+-S atac_rmdup.bw H3K27ac.bw H3K4me1.bw H3K4me3.bw H3K27me3.bw H3K9me3.bw H3K36me3.bw  \
+-R ~/wgbs/fastq_trim/fisher_neb_diff_und.bed --referencePoint center \
+--sortRegions descend -bs 100 -a 1000 -b 1000 -p 40 -out fisher_neb_diff_und.mat
+
+plotHeatmap --xAxisLabel "" --refPointLabel "CpG" --colorMap Blues -m fisher_neb_diff_und.mat -out atac_diff_undiff.pdf
