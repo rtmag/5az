2ffe603b3fb578887278999fb7bb4826bda56527
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-03-13 23:24:57 +0800

    Create downloads.bash

---
+++ downloads.bash
@@ -0,0 +1,5 @@
+~/myPrograms/sra-tools/sratoolkit.2.9.0-centos_linux64/bin/fastq-dump SRR5453772 --split-files
+~/myPrograms/sra-tools/sratoolkit.2.9.0-centos_linux64/bin/fastq-dump SRR5453767 --split-files
+###
+##
+#
