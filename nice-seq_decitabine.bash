621db89f016484bd89c08004b87c6f0e74d938b0
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-03-14 16:20:09 +0800

    Update nice-seq_decitabine.bash

--- nice-seq_decitabine.bash
+++ nice-seq_decitabine.bash
@@ -9,3 +9,12 @@ trim_galore --illumina --paired -o /root/decitabine/fastq \
 trim_galore --illumina --paired -o /root/decitabine/fastq \
 /root/decitabine/fastq/SRR5453772_1.fastq /root/decitabine/fastq/SRR5453772_2.fastq &
 
+STAR --genomeDir /root/resources/hg38_noanno/ --readFilesCommand zcat --runThreadN 40 \
+--readFilesIn /root/decitabine/fastq/ /root/decitabine/fastq/ \
+--alignIntronMax 1 --alignEndsType EndToEnd --outFilterMultimapNmax 1 --outFilterMatchNminOverLread 0.80 \
+--outSAMtype BAM SortedByCoordinate --outFileNamePrefix /root/decitabine/niceseq/niceseq_dec_
+
+STAR --genomeDir /root/resources/hg38_noanno/ --readFilesCommand zcat --runThreadN 40 \
+--readFilesIn /root/decitabine/fastq/ /root/decitabine/fastq/ \
+--alignIntronMax 1 --alignEndsType EndToEnd --outFilterMultimapNmax 1 --outFilterMatchNminOverLread 0.80 \
+--outSAMtype BAM SortedByCoordinate --outFileNamePrefix /root/decitabine/niceseq/niceseq_25k_
