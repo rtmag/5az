b3dd93784ff79e706831592f7c15c0dd45123c7f
Roberto Tirado Magallanes <rtmag4@gmail.com>
2019-02-26 18:18:26 +0800

    Create 1_trimming.bash

---
+++ analissa/1_trimming.bash
@@ -0,0 +1,115 @@
+#DT23_KG1A_NAD_5mM_48h
+#DT24_KG1A_Control
+#DT25_KG1A_NAD_5mM_72h
+#DT58_K562_AZA_2uM_48h
+#DT5_K562_NAD_10mM_48h
+#DT60_K562_AZA_2uM_72h
+#DT6_K562_Control
+#DT7_K562_NAD_10mM_72h
+#DT80_KG1A_AZA_2uM_48h
+#DT82_KG1A_AZA_2uM_72h
+
+cat /root/annalisa_wgbs/Clean/DT23/FCHNC75CCXY_L5_HUMfpgMAAAAAAA-55_1.fq.gz \
+/root/annalisa_wgbs/Clean/DT23/FCHNCCGCCXY_L7_HUMfpgMAAAAAAA-55_1.fq.gz \
+/root/annalisa_wgbs/Clean/DT23/FCHNCCGCCXY_L8_HUMfpgMAAAAAAA-55_1.fq.gz > \
+/root/annalisa_wgbs/Clean/DT23/KG1A_NAD_5mM_48h_1.fq.gz
+
+cat /root/annalisa_wgbs/Clean/DT23/FCHNC75CCXY_L5_HUMfpgMAAAAAAA-55_2.fq.gz \
+/root/annalisa_wgbs/Clean/DT23/FCHNCCGCCXY_L7_HUMfpgMAAAAAAA-55_2.fq.gz \
+/root/annalisa_wgbs/Clean/DT23/FCHNCCGCCXY_L8_HUMfpgMAAAAAAA-55_2.fq.gz > \
+/root/annalisa_wgbs/Clean/DT23/KG1A_NAD_5mM_48h_2.fq.gz
+
+cat /root/annalisa_wgbs/Clean/DT24/FCHNC75CCXY_L6_HUMfpgMAAAFAAA-63_1.fq.gz \
+/root/annalisa_wgbs/Clean/DT24/FCHNJLCCCXY_L7_HUMfpgMAAAFAAA-63_1.fq.gz \
+/root/annalisa_wgbs/Clean/DT24/FCHNJLCCCXY_L8_HUMfpgMAAAFAAA-63_1.fq.gz > \
+/root/annalisa_wgbs/Clean/DT24/DT24_KG1A_Control_1.fq.gz
+
+cat /root/annalisa_wgbs/Clean/DT24/FCHNC75CCXY_L6_HUMfpgMAAAFAAA-63_2.fq.gz \
+/root/annalisa_wgbs/Clean/DT24/FCHNJLCCCXY_L7_HUMfpgMAAAFAAA-63_2.fq.gz \
+/root/annalisa_wgbs/Clean/DT24/FCHNJLCCCXY_L8_HUMfpgMAAAFAAA-63_2.fq.gz > \
+/root/annalisa_wgbs/Clean/DT24/DT24_KG1A_Control_2.fq.gz
+###
+cat /root/annalisa_wgbs/Clean/DT25/FCHNC75CCXY_L6_HUMfpgMAAAJAAA-76_1.fq.gz \
+/root/annalisa_wgbs/Clean/DT25/FCHNJLCCCXY_L7_HUMfpgMAAAJAAA-76_1.fq.gz \
+/root/annalisa_wgbs/Clean/DT25/FCHNJLCCCXY_L8_HUMfpgMAAAJAAA-76_1.fq.gz > \
+/root/annalisa_wgbs/Clean/DT25/KG1A_NAD_5mM_72h_1.fq.gz
+
+cat /root/annalisa_wgbs/Clean/DT25/FCHNC75CCXY_L6_HUMfpgMAAAJAAA-76_2.fq.gz \
+/root/annalisa_wgbs/Clean/DT25/FCHNJLCCCXY_L7_HUMfpgMAAAJAAA-76_2.fq.gz \
+/root/annalisa_wgbs/Clean/DT25/FCHNJLCCCXY_L8_HUMfpgMAAAJAAA-76_2.fq.gz > \
+/root/annalisa_wgbs/Clean/DT25/KG1A_NAD_5mM_72h_2.fq.gz
+
+cat /root/annalisa_wgbs/Clean/DT58/FCHNC75CCXY_L4_HUMfpgMAAADAAA-60_1.fq.gz \
+/root/annalisa_wgbs/Clean/DT58/FCHNCCGCCXY_L5_HUMfpgMAAADAAA-60_1.fq.gz \
+/root/annalisa_wgbs/Clean/DT58/FCHNCCGCCXY_L6_HUMfpgMAAADAAA-60_1.fq.gz > \
+/root/annalisa_wgbs/Clean/DT58/K562_AZA_2uM_48h_1.fq.gz
+
+ cat /root/annalisa_wgbs/Clean/DT58/FCHNC75CCXY_L4_HUMfpgMAAADAAA-60_2.fq.gz \
+/root/annalisa_wgbs/Clean/DT58/FCHNCCGCCXY_L5_HUMfpgMAAADAAA-60_2.fq.gz \
+/root/annalisa_wgbs/Clean/DT58/FCHNCCGCCXY_L6_HUMfpgMAAADAAA-60_2.fq.gz > \
+/root/annalisa_wgbs/Clean/DT58/K562_AZA_2uM_48h_2.fq.gz
+
+cat /root/annalisa_wgbs/Clean/DT5/FCHNCCGCCXY_L3_HUMfpgMAAABAAA-57_1.fq.gz \
+/root/annalisa_wgbs/Clean/DT5/FCHNCCGCCXY_L4_HUMfpgMAAABAAA-57_1.fq.gz \
+/root/annalisa_wgbs/Clean/DT5/FCHNFCWCCXY_L8_HUMfpgMAAABAAA-57_1.fq.gz > \
+/root/annalisa_wgbs/Clean/DT5/K562_NAD_10mM_48h_1.fq.gz
+
+cat /root/annalisa_wgbs/Clean/DT5/FCHNCCGCCXY_L3_HUMfpgMAAABAAA-57_2.fq.gz \
+/root/annalisa_wgbs/Clean/DT5/FCHNCCGCCXY_L4_HUMfpgMAAABAAA-57_2.fq.gz \
+/root/annalisa_wgbs/Clean/DT5/FCHNFCWCCXY_L8_HUMfpgMAAABAAA-57_2.fq.gz > \
+/root/annalisa_wgbs/Clean/DT5/K562_NAD_10mM_48h_2.fq.gz
+
+cat /root/annalisa_wgbs/Clean/DT60/FCHNC75CCXY_L5_HUMfpgMAAAEAAA-61_1.fq.gz \
+/root/annalisa_wgbs/Clean/DT60/FCHNCCGCCXY_L7_HUMfpgMAAAEAAA-61_1.fq.gz \
+/root/annalisa_wgbs/Clean/DT60/FCHNCCGCCXY_L8_HUMfpgMAAAEAAA-61_1.fq.gz > \
+/root/annalisa_wgbs/Clean/DT60/K562_AZA_2uM_72h_1.fq.gz
+
+cat /root/annalisa_wgbs/Clean/DT60/FCHNC75CCXY_L5_HUMfpgMAAAEAAA-61_2.fq.gz \
+/root/annalisa_wgbs/Clean/DT60/FCHNCCGCCXY_L7_HUMfpgMAAAEAAA-61_2.fq.gz \
+/root/annalisa_wgbs/Clean/DT60/FCHNCCGCCXY_L8_HUMfpgMAAAEAAA-61_2.fq.gz > \
+/root/annalisa_wgbs/Clean/DT60/K562_AZA_2uM_72h_2.fq.gz
+###################################################################################
+cat /root/annalisa_wgbs/Clean/DT6/FCHNCCGCCXY_L3_HUMfpgMAAACAAA-58_1.fq.gz \
+/root/annalisa_wgbs/Clean/DT6/FCHNCCGCCXY_L4_HUMfpgMAAACAAA-58_1.fq.gz \
+/root/annalisa_wgbs/Clean/DT6/FCHNFCWCCXY_L8_HUMfpgMAAACAAA-58_1.fq.gz > \
+/root/annalisa_wgbs/Clean/DT6/K562_Control_1.fq.gz
+
+cat /root/annalisa_wgbs/Clean/DT6/FCHNCCGCCXY_L3_HUMfpgMAAACAAA-58_2.fq.gz \
+/root/annalisa_wgbs/Clean/DT6/FCHNCCGCCXY_L4_HUMfpgMAAACAAA-58_2.fq.gz \
+/root/annalisa_wgbs/Clean/DT6/FCHNFCWCCXY_L8_HUMfpgMAAACAAA-58_2.fq.gz > \
+/root/annalisa_wgbs/Clean/DT6/K562_Control_2.fq.gz
+
+cat /root/annalisa_wgbs/Clean/DT7/FCHNC75CCXY_L4_HUMfpgMAAAHAAA-66_1.fq.gz \
+/root/annalisa_wgbs/Clean/DT7/FCHNCCGCCXY_L5_HUMfpgMAAAHAAA-66_1.fq.gz \
+/root/annalisa_wgbs/Clean/DT7/FCHNCCGCCXY_L6_HUMfpgMAAAHAAA-66_1.fq.gz > \
+/root/annalisa_wgbs/Clean/DT7/K562_NAD_10mM_72h_1.fq.gz
+
+cat /root/annalisa_wgbs/Clean/DT7/FCHNC75CCXY_L4_HUMfpgMAAAHAAA-66_2.fq.gz \
+/root/annalisa_wgbs/Clean/DT7/FCHNCCGCCXY_L5_HUMfpgMAAAHAAA-66_2.fq.gz \
+/root/annalisa_wgbs/Clean/DT7/FCHNCCGCCXY_L6_HUMfpgMAAAHAAA-66_2.fq.gz > \
+/root/annalisa_wgbs/Clean/DT7/K562_NAD_10mM_72h_2.fq.gz
+
+cat /root/annalisa_wgbs/Clean/DT80/FCHNC75CCXY_L7_HUMfpgMAAAIAAA-67_1.fq.gz \
+/root/annalisa_wgbs/Clean/DT80/FCHNK72CCXY_L1_HUMfpgMAAAIAAA-67_1.fq.gz \
+/root/annalisa_wgbs/Clean/DT80/FCHNK72CCXY_L2_HUMfpgMAAAIAAA-67_1.fq.gz > \
+/root/annalisa_wgbs/Clean/DT80/KG1A_AZA_2uM_48h_1.fq.gz
+
+cat /root/annalisa_wgbs/Clean/DT80/FCHNC75CCXY_L7_HUMfpgMAAAIAAA-67_2.fq.gz \
+/root/annalisa_wgbs/Clean/DT80/FCHNK72CCXY_L1_HUMfpgMAAAIAAA-67_2.fq.gz \
+/root/annalisa_wgbs/Clean/DT80/FCHNK72CCXY_L2_HUMfpgMAAAIAAA-67_2.fq.gz > \
+/root/annalisa_wgbs/Clean/DT80/KG1A_AZA_2uM_48h_2.fq.gz
+
+cat /root/annalisa_wgbs/Clean/DT82/FCHNC75CCXY_L7_HUMfpgMAAAGAAA-65_1.fq.gz \
+/root/annalisa_wgbs/Clean/DT82/FCHNK72CCXY_L1_HUMfpgMAAAGAAA-65_1.fq.gz \
+/root/annalisa_wgbs/Clean/DT82/FCHNK72CCXY_L2_HUMfpgMAAAGAAA-65_1.fq.gz > \
+/root/annalisa_wgbs/Clean/DT82/KG1A_AZA_2uM_72h_1.fq.gz
+
+cat /root/annalisa_wgbs/Clean/DT82/FCHNC75CCXY_L7_HUMfpgMAAAGAAA-65_2.fq.gz \
+/root/annalisa_wgbs/Clean/DT82/FCHNK72CCXY_L1_HUMfpgMAAAGAAA-65_2.fq.gz \
+/root/annalisa_wgbs/Clean/DT82/FCHNK72CCXY_L2_HUMfpgMAAAGAAA-65_2.fq.gz > \
+/root/annalisa_wgbs/Clean/DT82/KG1A_AZA_2uM_72h_2.fq.gz
+###############################################################################################
+trim_galore --paired --non_directional -o /root/annalisa_wgbs/trimmed/ \
+/root/annalisa_wgbs/trimmed/P007_48h_doxo_1.fq.gz \
+/root/annalisa_wgbs/trimmed/P007_48h_doxo_2.fq.gz &
+
