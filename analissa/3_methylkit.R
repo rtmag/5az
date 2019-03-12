fe4e8d04b202631a0d43db756412dd69532d1f7f
Roberto Tirado Magallanes <rtmag4@gmail.com>
2019-03-12 13:28:21 +0800

    Update 3_methylkit.R

--- analissa/3_methylkit.R
+++ analissa/3_methylkit.R
@@ -1,31 +1,10 @@
 
 library(methylKit)
 
-file.list=list(
-"/root/annalisa_wgbs/bismark_report/DT24_KG1A_Control/DT24_r1_bismark_bt2_pe.CpG_report.txt",
-"/root/annalisa_wgbs/bismark_report/DT58_K562_AZA_2uM_48h/DT58_r1_bismark_bt2_pe.CpG_report.txt",
-"/root/annalisa_wgbs/bismark_report/DT60_K562_AZA_2uM_72h/DT60_r1_bismark_bt2_pe.CpG_report.txt",
-"/root/annalisa_wgbs/bismark_report/DT6_K562_Control/DT6_r1_bismark_bt2_pe.CpG_report.txt",
-"/root/annalisa_wgbs/bismark_report/DT80_KG1A_AZA_2uM_48h/DT80_r1_bismark_bt2_pe.CpG_report.txt",
-"/root/annalisa_wgbs/bismark_report/DT82_KG1A_AZA_2uM_72h/DT82_r1_bismark_bt2_pe.CpG_report.txt")
-
-
-# read the files to a methylRawList object: myobj
-myobj=methRead(file.list,
-           sample.id=list("KG1A_CTRL","KG1A_2uM","KG1A_5uM","K562_CTRL","K562_2uM","K562_5uM"),
-           assembly="hg38",
-           treatment=c(1,2,3,4,5,6),
-           context="CpG",
-           pipeline="bismarkCytosineReport",
-           header=FALSE,
-           mincov=4)
-
-meth=unite(myobj, destrand=TRUE,mc.cores=40)
-
 ######################## KG1A CTRL VS KG1A 48h ###########################################################
 file.list=list(
 "/root/annalisa_wgbs/bismark_report/DT24_KG1A_Control/DT24_r1_bismark_bt2_pe.CpG_report.txt",
-"/root/annalisa_wgbs/bismark_report/DT58_K562_AZA_2uM_48h/DT58_r1_bismark_bt2_pe.CpG_report.txt")
+"/root/annalisa_wgbs/bismark_report/DT80_KG1A_AZA_2uM_48h/DT80_r1_bismark_bt2_pe.CpG_report.txt")
    
 myobj=methRead(file.list,
            sample.id=list("KG1A_CTRL","KG1A_48h"),
@@ -44,7 +23,7 @@ saveRDS(fisher_neb,"KG1A_CTRL_VS_48h.rds")
 ######################## KG1A CTRL VS KG1A 72h ###########################################################
 file.list=list(
 "/root/annalisa_wgbs/bismark_report/DT24_KG1A_Control/DT24_r1_bismark_bt2_pe.CpG_report.txt",
-"/root/annalisa_wgbs/bismark_report/DT60_K562_AZA_2uM_72h/DT60_r1_bismark_bt2_pe.CpG_report.txt")
+"/root/annalisa_wgbs/bismark_report/DT82_KG1A_AZA_2uM_72h/DT82_r1_bismark_bt2_pe.CpG_report.txt")
 
 myobj=methRead(file.list,
            sample.id=list("KG1A_CTRL","KG1A_72h"),
