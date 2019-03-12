997e7fc089bc0296775a20de4ebd17077713561f
Roberto Tirado Magallanes <rtmag4@gmail.com>
2019-03-12 13:23:48 +0800

    Update 3_methylkit.R

--- analissa/3_methylkit.R
+++ analissa/3_methylkit.R
@@ -22,13 +22,13 @@ myobj=methRead(file.list,
 
 meth=unite(myobj, destrand=TRUE,mc.cores=40)
 
-######################## KG1A CTRL VS KG1A 2uM ###########################################################
+######################## KG1A CTRL VS KG1A 48h ###########################################################
 file.list=list(
 "/root/annalisa_wgbs/bismark_report/DT24_KG1A_Control/DT24_r1_bismark_bt2_pe.CpG_report.txt",
 "/root/annalisa_wgbs/bismark_report/DT58_K562_AZA_2uM_48h/DT58_r1_bismark_bt2_pe.CpG_report.txt")
    
 myobj=methRead(file.list,
-           sample.id=list("KG1A_CTRL","KG1A_2uM"),
+           sample.id=list("KG1A_CTRL","KG1A_48h"),
            assembly="hg38",
            treatment=c(0,1),
            context="CpG",
@@ -38,5 +38,63 @@ myobj=methRead(file.list,
 
 meth=unite(myobj, destrand=TRUE,mc.cores=40)
 pooled.myDiff=calculateDiffMeth(meth,num.cores=40)
+methData=getData(meth)
+fisher_neb=data.frame(pooled.myDiff,KG1A_CTRL=(methData[,6]/methData[,5]),KG1A_48h=(methData[,9]/methData[,8]) ) 
+saveRDS(fisher_neb,"KG1A_CTRL_VS_48h.rds")
+######################## KG1A CTRL VS KG1A 72h ###########################################################
+file.list=list(
+"/root/annalisa_wgbs/bismark_report/DT24_KG1A_Control/DT24_r1_bismark_bt2_pe.CpG_report.txt",
+"/root/annalisa_wgbs/bismark_report/DT60_K562_AZA_2uM_72h/DT60_r1_bismark_bt2_pe.CpG_report.txt")
 
+myobj=methRead(file.list,
+           sample.id=list("KG1A_CTRL","KG1A_72h"),
+           assembly="hg38",
+           treatment=c(0,1),
+           context="CpG",
+           pipeline="bismarkCytosineReport",
+           header=FALSE,
+           mincov=4)
 
+meth=unite(myobj, destrand=TRUE,mc.cores=40)
+pooled.myDiff=calculateDiffMeth(meth,num.cores=40)
+methData=getData(meth)
+fisher_neb=data.frame(pooled.myDiff,KG1A_CTRL=(methData[,6]/methData[,5]),KG1A_72h=(methData[,9]/methData[,8]) ) 
+saveRDS(fisher_neb,"KG1A_CTRL_VS_72h.rds")
+######################## K562 CTRL VS K562 72h ###########################################################
+file.list=list(
+"/root/annalisa_wgbs/bismark_report/DT6_K562_Control/DT6_r1_bismark_bt2_pe.CpG_report.txt",
+"/root/annalisa_wgbs/bismark_report/DT58_K562_AZA_2uM_48h/DT58_r1_bismark_bt2_pe.CpG_report.txt")
+   
+myobj=methRead(file.list,
+           sample.id=list("K562_CTRL","K562_48h"),
+           assembly="hg38",
+           treatment=c(0,1),
+           context="CpG",
+           pipeline="bismarkCytosineReport",
+           header=FALSE,
+           mincov=4)
+
+meth=unite(myobj, destrand=TRUE,mc.cores=40)
+pooled.myDiff=calculateDiffMeth(meth,num.cores=40)
+methData=getData(meth)
+fisher_neb=data.frame(pooled.myDiff,K562_CTRL=(methData[,6]/methData[,5]),K562_48h=(methData[,9]/methData[,8]) ) 
+saveRDS(fisher_neb,"K562_CTRL_VS_48h.rds")
+######################## K562 CTRL VS K562 72h ###########################################################
+file.list=list(
+"/root/annalisa_wgbs/bismark_report/DT6_K562_Control/DT6_r1_bismark_bt2_pe.CpG_report.txt",
+"/root/annalisa_wgbs/bismark_report/DT60_K562_AZA_2uM_72h/DT60_r1_bismark_bt2_pe.CpG_report.txt")
+
+myobj=methRead(file.list,
+           sample.id=list("K562_CTRL","K562_72h"),
+           assembly="hg38",
+           treatment=c(0,1),
+           context="CpG",
+           pipeline="bismarkCytosineReport",
+           header=FALSE,
+           mincov=4)
+
+meth=unite(myobj, destrand=TRUE,mc.cores=40)
+pooled.myDiff=calculateDiffMeth(meth,num.cores=40)
+methData=getData(meth)
+fisher_neb=data.frame(pooled.myDiff,K562_CTRL=(methData[,6]/methData[,5]),K562_72h=(methData[,9]/methData[,8]) ) 
+saveRDS(fisher_neb,"K562_CTRL_VS_72h.rds")
