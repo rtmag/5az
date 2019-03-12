2f44b306be8fb207a69daf1b3ecc747e2b2a0083
Roberto Tirado Magallanes <rtmag4@gmail.com>
2019-03-12 19:43:17 +0800

    Update GSE104837_RNASEQ.R

--- 5az_metadata/GSE104837_RNASEQ.R
+++ 5az_metadata/GSE104837_RNASEQ.R
@@ -1,2 +1,8 @@
-data = read.table("more GSE104837_YB5_HH1_10uM_summary.txt |head -n6|cut -f1,26-28,38-40,56-57",header=T,sep="\t")
+data = read.table(pipe("more GSE104837_YB5_HH1_10uM_summary.txt |cut -f1,26-28,38-40,56-57"),header=T,sep="\t")
+
+
+plot(data[,8],-log10(data[,9]),pch=20)
+abline(h=-log10(0.05),lty=2)
+abline(v=-1,lty=2)
+abline(v=1,lty=2)
 
