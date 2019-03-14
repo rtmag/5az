e4c4aced27d961447f0a8a0c993a82ba494aad42
Roberto Tirado Magallanes <rtmag4@gmail.com>
2019-03-15 06:12:15 +0800

    Update 4_decitabine_res.R

--- analissa/4_decitabine_res.R
+++ analissa/4_decitabine_res.R
@@ -1,1 +1,23 @@
-KG1A_CTRL_VS_48h = readRDS("KG1A_CTRL_VS_48h.rds")
+fisher_neb = readRDS("KG1A_CTRL_VS_48h.rds")
+diff=fisher_neb[(fisher_neb[,6]<0.05 & fisher_neb[,7]<(-50)),]
+dim(diff) # 10804
+undiff=fisher_neb[ fisher_neb[,7]>(-10)  &  rowMeans(cbind(fisher_neb[,8],fisher_neb[,9]))>.90 ,]
+dim(undiff) # 7654032
+######################################################################################################
+fisher_neb = readRDS("KG1A_CTRL_VS_72h.rds")
+diff=fisher_neb[(fisher_neb[,6]<0.05 & fisher_neb[,7]<(-50)),]
+dim(diff) # 15072
+undiff=fisher_neb[ fisher_neb[,7]>(-10)  &  rowMeans(cbind(fisher_neb[,8],fisher_neb[,9]))>.90 ,]
+dim(undiff) # 7410582
+######################################################################################################
+fisher_neb = readRDS("K562_CTRL_VS_48h.rds")
+diff=fisher_neb[(fisher_neb[,6]<0.05 & fisher_neb[,7]<(-50)),]
+dim(diff) # 245721
+undiff=fisher_neb[ fisher_neb[,7]>(-10)  &  rowMeans(cbind(fisher_neb[,8],fisher_neb[,9]))>.90 ,]
+dim(undiff) # 1318947
+######################################################################################################
+fisher_neb = readRDS("K562_CTRL_VS_72h.rds")
+diff=fisher_neb[(fisher_neb[,6]<0.05 & fisher_neb[,7]<(-50)),]
+dim(diff) # 462798
+undiff=fisher_neb[ fisher_neb[,7]>(-10)  &  rowMeans(cbind(fisher_neb[,8],fisher_neb[,9]))>.90 ,]
+dim(undiff) # 1189724
