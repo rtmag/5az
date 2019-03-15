66e78b2cd7b331910205800845318abfa53e0ad4
Roberto Tirado Magallanes <rtmag4@gmail.com>
2019-03-15 11:32:35 +0800

    Create download.bash

---
+++ 5aza_p53/download.bash
@@ -0,0 +1,16 @@
+#######################################################################################
+~/myPrograms/sra-tools/sratoolkit.2.9.0-centos_linux64/bin/fastq-dump --split-files \
+-Z SRR5926789 > SRR5926789_MCF7_Decitabine_ATACseq.fastq 
+
+~/myPrograms/sra-tools/sratoolkit.2.9.0-centos_linux64/bin/fastq-dump --split-files \
+-Z SRR5926790 > SRR5926790_MCF7_minus_Decitabine_p53chip.fastq 
+
+~/myPrograms/sra-tools/sratoolkit.2.9.0-centos_linux64/bin/fastq-dump --split-files \
+-Z SRR5926791 > SRR5926791_MCF7_plus_Decitabine_p53chip.fastq 
+
+~/myPrograms/sra-tools/sratoolkit.2.9.0-centos_linux64/bin/fastq-dump --split-files \
+-Z SRR5724756 > SRR5724756_MCF7_ATACseq.fastq 
+
+~/myPrograms/sra-tools/sratoolkit.2.9.0-centos_linux64/bin/fastq-dump --split-files \
+-Z SRR5724759 > SRR5724759_MCF7_Input.fastq 
+#######################################################################################
